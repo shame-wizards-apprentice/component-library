702a3957eaa3c3170f4b936fae82e2023ce993ff
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-06-02 14:06:53 -0700

    created and styled logo component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { GlobalStyle } from './utils';
 import { Button } from './components/Button';
+import { Logo } from './components/Logo';
 
 const App = () => {
   return (
     <main>
     <GlobalStyle />
-    <Button>This is a primary button</Button>
+    <Logo />
+    <br/>
+    <br/>
+    <Logo variant='secondary' href='#' />
+    <br/>
+    <br/>
+    <Button href='http://google.com'>This is a button with a link</Button>
     <br />
     <br />
     <Button variant='secondary' size='large' disabled>
-      This is a large secondary button
+      This is a secondary disabled button
     </Button>
     <br />
     <br />
     <Button variant='tertiary' size='small'>
-      This is a small tertiary button
+      This is a tertiary button
     </Button>
   </main>
   );
